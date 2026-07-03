789a3e2d95ffc179051f4d0413422c5e3bd73b78
Elliott Richmond <elliott@squareonemd.co.uk>
2026-07-03 12:12:48 +0100

    sleeps min/max fix

--- patterns/house-card-search-special-offer.php
+++ patterns/house-card-search-special-offer.php
@@ -20,20 +20,17 @@
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"top":{"color":"var:preset|color|tertiary","width":"1px"},"right":[],"bottom":[],"left":[]}},"layout":{"type":"constrained","justifyContent":"center"}} -->
 <div class="wp-block-group" style="border-top-color:var(--wp--preset--color--tertiary);border-top-width:1px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0.2em"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"x-small"} -->
-<p class="has-x-small-font-size">Sleeps </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"core/post-meta","args":{"key":"sleeps_min"}}}},"fontSize":"x-small"} -->
-<p class="has-x-small-font-size"></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"fontSize":"x-small"} -->
-<p class="has-x-small-font-size"> to </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"core/post-meta","args":{"key":"sleeps_max"}}}},"fontSize":"x-small"} -->
-<p class="has-x-small-font-size"></p>
+<div class="wp-block-group"><?php
+// Build the "Sleeps" range from post meta. Some houses have no sleeps_min,
+// which previously rendered "Sleeps  to N"; in that case show just "Sleeps N".
+$sleeps_id  = get_the_ID();
+$sleeps_min = get_post_meta($sleeps_id, 'sleeps_min', true);
+$sleeps_max = get_post_meta($sleeps_id, 'sleeps_max', true);
+$sleeps_text = ('' !== trim((string) $sleeps_min))
+	? sprintf('Sleeps %s to %s', $sleeps_min, $sleeps_max)
+	: sprintf('Sleeps %s', $sleeps_max);
+?><!-- wp:paragraph {"fontSize":"x-small"} -->
+<p class="has-x-small-font-size"><?php echo esc_html($sleeps_text); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
