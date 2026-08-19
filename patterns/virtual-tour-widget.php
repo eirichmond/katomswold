12878c0c7aae0ef8eb015ea42f242d6e562b078f
Elliott Richmond <elliott@squareonemd.co.uk>
2026-08-19 10:47:35 +0100

    virtual tour widget added

---
+++ patterns/virtual-tour-widget.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Virtual Tour Widget
+ * Slug: katomswold/virtual-tour-widget
+ * Categories: banner
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["kate-and-toms"],"name":"Standard Widget Large Image","patternName":"core/block/149323"},"align":"full","style":{"dimensions":{"minHeight":"3px"},"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"},"padding":{"bottom":"var:preset|spacing|50","right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|50"}}},"backgroundColor":"colorthirteen","layout":{"type":"constrained"},"anchor":"h-virtual-tour"} -->
+<div class="wp-block-group alignfull has-colorthirteen-background-color has-background" id="h-virtual-tour" style="min-height:3px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"textAlign":"center","fontStyle":"normal","fontWeight":"300"},"spacing":{"padding":{"top":"0"},"margin":{"bottom":"var:preset|spacing|30"}},"elements":{"link":{"color":{"text":"var:preset|color|charcoal"}}}},"textColor":"charcoal","fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-charcoal-color has-text-color has-link-color has-x-large-font-size" style="margin-bottom:var(--wp--preset--spacing--30);padding-top:0;font-style:normal;font-weight:300">Virtual Tour</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"textAlign":"center"},"elements":{"link":{"color":{"text":"var:preset|color|charcoal"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"textColor":"charcoal","fontSize":"small"} -->
+<p class="has-text-align-center has-charcoal-color has-text-color has-link-color has-small-font-size" style="padding-bottom:var(--wp--preset--spacing--40)">See the house from every angle with our immersive 360° virtual tour. Step inside and explore the living spaces, bedrooms, dining rooms, kitchens and bathrooms at your own pace, getting a real feel for the layout and character of the house before you arrive.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:kate-toms-core/vr-tour {"tourUrl":"https://my.matterport.com/show/?m=hdTDNMHgBC1\u0026brand=0","align":"full"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"categories":["banner"],"patternName":"katomswold/button-widget","name":"Button Widget"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|40","bottom":"var:preset|spacing|20"}}},"backgroundColor":"colornine","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-colornine-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:buttons {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"colorfive"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-colorfive-background-color has-background wp-element-button" href="{perma}">Back to House </a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"colorthree"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-colorthree-background-color has-background wp-element-button" href="{perma}">Key Facts</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"colortwo"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-colortwo-background-color has-background wp-element-button" href="{perma}">Book Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
